f22d3b6a5448f6a026740825daf79dae44864078
Lavinia <lavinia271208@gmail.com>
2026-08-29 09:49:54 -0300

    correção das rotas

--- routes/web.php
+++ routes/web.php
@@ -29,46 +29,46 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->midd
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
 
 
-// Dashboard
-Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    // Dashboard
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 
-Route::delete('/agendamentos/{id}', [AgendamentoController::class, 'destroy'])->name('admin.lista_agendamentos.destroy');
-    
-// serviços
-Route::get('/servicos', [AdminController::class, 'servicos'])->name('admin.servicos');
+    Route::delete('/agendamentos/{id}', [AgendamentoController::class, 'destroy'])->name('admin.lista_agendamentos.destroy');
+        
+    // serviços
+    Route::get('/servicos', [AdminController::class, 'servicos'])->name('admin.servicos');
 
-Route::post('/servicos', [AdminController::class, 'storeServico'])->name('admin.servicos.store');
+    Route::post('/servicos', [AdminController::class, 'storeServico'])->name('admin.servicos.store');
 
-Route::get('/servicos/{id}/edit', [AdminController::class, 'editServico'])->name('admin.servicos.edit');
+    Route::get('/servicos/{id}/edit', [AdminController::class, 'editServico'])->name('admin.servicos.edit');
 
-Route::put('/servicos/{id}', [AdminController::class, 'updateServico'])->name('admin.servicos.update');
+    Route::put('/servicos/{id}', [AdminController::class, 'updateServico'])->name('admin.servicos.update');
 
-Route::delete('/servicos/{id}', [AdminController::class, 'destroyServico'])->name('admin.servicos.destroy');
+    Route::delete('/servicos/{id}', [AdminController::class, 'destroyServico'])->name('admin.servicos.destroy');
 
 
-// Agendamenos
-    
-// Lista
-Route::get('/lista-agendamentos', [AdminController::class, 'listaAgendamentos'])->name('admin.lista_agendamentos');
+    // Agendamenos
+        
+    // Lista
+    Route::get('/lista-agendamentos', [AdminController::class, 'listaAgendamentos'])->name('admin.lista_agendamentos');
 
-// Criar
-Route::get('/agendamentos/create', [AgendamentoController::class, 'createAgendamento'])->name('admin.lista_agendamentos.create');
+    // Criar
+    Route::get('/agendamentos/create', [AgendamentoController::class, 'createAgendamento'])->name('admin.lista_agendamentos.create');
 
-// Salvar
-Route::post('/agendamentos', [AgendamentoController::class, 'storeAdmin'])->name('admin.lista_agendamentos.store');
+    // Salvar
+    Route::post('/agendamentos', [AgendamentoController::class, 'storeAdmin'])->name('admin.lista_agendamentos.store');
 
-// Editar
-Route::put('/agendamentos/{id}', [AgendamentoController::class, 'update'])->name('admin.lista_agendamentos.update');
+    // Editar
+    Route::put('/agendamentos/{id}', [AgendamentoController::class, 'update'])->name('admin.lista_agendamentos.update');
 
-// full calendar    
-Route::get('/admin/agendamentos/events', [AgendamentoController::class, 'getEvents'])->name('admin.agendamentos.events');
+    // full calendar    
+    Route::get('/admin/agendamentos/events', [AgendamentoController::class, 'getEvents'])->name('admin.agendamentos.events');
 
-// bloqueio de horários
-Route::get('/bloqueios', [HorarioBloqueadoController::class, 'index'])->name('admin.bloqueios.index');
+    // bloqueio de horários
+    Route::get('/bloqueios', [HorarioBloqueadoController::class, 'index'])->name('admin.bloqueios.index');
 
-Route::post('/bloqueios', [HorarioBloqueadoController::class, 'store'])->name('admin.bloqueios.store');
+    Route::post('/bloqueios', [HorarioBloqueadoController::class, 'store'])->name('admin.bloqueios.store');
 
-Route::delete('/bloqueios/{id}', [HorarioBloqueadoController::class, 'destroy'])->name('admin.bloqueios.destroy');
+    Route::delete('/bloqueios/{id}', [HorarioBloqueadoController::class, 'destroy'])->name('admin.bloqueios.destroy');
 
-Route::put('/bloqueios/{id}', [HorarioBloqueadoController::class, 'update'])->name('admin.bloqueios.update');
+    Route::put('/bloqueios/{id}', [HorarioBloqueadoController::class, 'update'])->name('admin.bloqueios.update');
 });
